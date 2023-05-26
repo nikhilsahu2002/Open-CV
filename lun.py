da9a01d08f5cfd82f1cb5bfa794d3b6c1491e71b
sagr-456 <sagarsgs456@gmail.com>
2023-05-26 12:30:02 +0530

    sss

--- lun.py
+++ lun.py
@@ -3,9 +3,22 @@ client=pymongo.MongoClient('mongodb://localhost:27017/?directConnection=true')
 my=client['DB']
 info=my.tab
 res=[{
-    'id':102,
-    'name':'abhay',
-    'image':'imag1.png'
+    'id':103,
+    'name':'a'
+},
+{
+    'id':104,
+    'name':'ab'
+
+},{
+    'id':105,
+    'name':'bhay'
 
 }]
-info.insert_many(res)
+# info.insert_many(res)
+# z=info.find_one({},{'_id':0})
+
+z=info.find({},{'_id':0})
+for document in z:
+    # Process each document as needed
+    print(document)
